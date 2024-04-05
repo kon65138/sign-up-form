7685919aad17ecc9c68e2fc1d366cc2dc34868a6
kon <156806318+kon65138@users.noreply.github.com>
2024-04-05 17:49:29 +0100

    flesh out form

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-console.log('hello');
+const form = document.querySelector("form");
+
+form.addEventListener('submit', (e) => {
+    let password = document.getElementById("password");
+    let confPassword = document.getElementById("confPassword");
+    let msg = document.getElementById("pMatch");
+    
+    if (password.length != 0) {
+        if (password != confPassword) {
+            e.preventDefault();
+            msg.textContent = "Passwords do not match";
+        }
+    }
+})
